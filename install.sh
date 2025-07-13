8ba0ff698ca3e8703feec734cf67eac56edad333
Justin Burrous <justinmburrous@gmail.com>
2025-07-12 22:51:48 -0700

    Add sudo for pacman operations for arch install

--- install.sh
+++ install.sh
@@ -198,13 +198,13 @@ function brew_install(){
 function arch_install(){
   echo "Running Arch install"
 
-  pacman -Syu
+  sudo pacman -Syu
 
   echo "Checking $arch_packages  list"
   arch_packages=$( basedir )/package_lists/arch.txt
 
   while read package_name; do
-    payman -S $package_name
+    sudo payman -S $package_name
   done < $arch_packages
 
   echo "Done Arch install"
