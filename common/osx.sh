71028aa22309a98b8e6575699e70629e767038e9
Justin Burrous <justinmburrous@gmail.com>
2025-08-12 07:04:20 -0700

    Switch to Brewfile for homebrew package install

--- common/osx.sh
+++ common/osx.sh
@@ -1,21 +1,11 @@
 function brew_install(){
-  echo "Updating homebrew"
+  echo "Updating Homebrew"
   brew update
 
   echo "Doing Homebrew install"
-  BREW_DESIRED_PACKAGES="$( basedir )/package_lists/brew_packages.txt"
+  BREWFILE="$( basedir )/package_lists/brew_packages.txt"
 
-  set +e # checks for install via exit code
-  BREW_INSTALLED_PACKAGES=$(brew list)
-  while read package_name; do
-    if brew ls --versions $package_name; then
-      echo "$package_name already installed"
-    else
-      echo "Installing: $package_name"
-      brew install $package_name
-    fi
-  done < $BREW_DESIRED_PACKAGES
-  set -e
+  brew bundle install --upgrade --file $BREWFILE
 
   echo "Running brew doctor"
   brew doctor
